7235c258bb47a48cd27572cc24286760f9cfbe65
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-05-14 23:45:36 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,26 @@
+---------------------------------------------------------------
+
+-- Welcome to the Main Script of UXMB: PS4 Edition.
+-- Developer(s): -ROM-PacMe
+--               -KillingName
+
+---------------------------------------------------------------
+---------------------------------------------------------------
+
+-- Bienvenido al Script Principal de UXMB: PS4 Edition.
+-- Desarrollador(es): -ROM-PacMe
+--                    -KillingName
+
+---------------------------------------------------------------
+
+-- Documentación: ONELua for PSP V4 Release 1
+-- Link: http://onelua.x10.mx/psp/docs/es/index.html
+
+---------------------------------------------------------------
+
+-- ## Enter to Recovery Mode ## 
+-- ## Entrar al Modo Recovery / Recuperación ##
+
+---------------------------------------------------------------
+
+dofile("uxmb_recovery.pacme")
